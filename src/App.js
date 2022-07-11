8c9d075a8f12a51dbf8478cefaf040dd54703100
Florian Mabillon <mabillon.flo@gmail.com>
2022-07-11 10:34:40 +0200

    version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopBar from "./components/TopBar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Policy from "./components/Policy";
+import NavBar from './components/NavBar';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <TopBar/>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<HomeScreen/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/Policy' element={<Policy/>}></Route>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import { Card, Button, Row, Col } from 'react-bootstrap';
+
+
+const Pizza = (props) => {
+    const [taille, setTaille] = useState('small');
+    const [quantite, setQuantite] = useState(1); 
+  return (
+    <Card style={{width:"18rem"}}>
+        <Card.Img variant="top" src={props.lapizza.image} />
+        <Card.Body>
+            <Card.Title>{props.lapizza.name}</Card.Title>
+            <Card.Text>
+                <Row>
+                    <Col md={6}>
+                        <h6>Taille:
+                        <select value={taille} onChange={(e) => setTaille(e.target.value)}>
+                            {props.lapizza.varients.map(taille => (
+                                <option>{taille}</option>
+                            ))}
+                        </select>
+                        </h6>
+                    </Col>
+                    <Col md={6}>
+                        <h6>Quantité :<br/>
+                                <select value={quantite} onChange={(e) => setQuantite(e.target.value)}>
+                                    {[...Array(10).keys()].map((v,i) => (
+                                        <option>{i+1}</option>
+                                    ))}
+                                </select>
+                        </h6>
+                    </Col>
+                </Row>
+            </Card.Text>
+            <Row>
+                <Col>
+                    Prix : {props.lapizza.prices[0][taille] * quantite} €
+                </Col>
+                <Col>
+                <Button className="bg-warning text-light">Add</Button>
+                </Col>
+            </Row>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default Pizza;
